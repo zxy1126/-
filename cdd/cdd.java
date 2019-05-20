f30d2df65b8782d734b972b4475551f7a57b7ac5
cdd <2998950770@qq.com>
2019-05-20 23:18:08 +0800

    陈丹丹

--- cdd/cdd.java
+++ cdd/cdd.java
@@ -13,3 +13,18 @@ public class Emp{
         }
     }
 }
+//陈丹丹
+public class Emp{
+    private volatile static Emp  emp=null;
+    private Emp(){
+        public static Emp getEmp(){
+            if (emp=null){
+                synchronized (Emp.class){
+                    if (emp==null){
+                        emp=new Emp();
+                    }
+                }
+            }return emp;
+        }
+    }
+}
